16b5b71b66ef13639fb7330d2702ef6a7e90f1a6
funnsam <funnsam8@gmail.com>
2024-05-06 19:14:15 +0800

    mnist decoding

--- mnist/src/main.rs
+++ mnist/src/main.rs
@@ -1,5 +1,32 @@
+use smolmatrix::*;
+
 mod reader;
 
 fn main() {
-    println!("Hello, world!");
+    let images = std::fs::read("db/t10k-images-idx3-ubyte").unwrap();
+    let images = reader::read_images(&mut images.as_slice()).unwrap();
+    let labels = std::fs::read("db/t10k-labels-idx1-ubyte").unwrap();
+    let labels = reader::read_labels(&mut labels.as_slice()).unwrap();
+
+    for i in 0.. {
+        visualize(&images[i]);
+        println!("{}", labels[i]);
+        std::thread::sleep_ms(1000);
+    }
+}
+
+fn visualize(fb: &Vector<784>) {
+    for yhalf in 0..28/2 {
+        for x in 0..28 {
+            plot(fb[(0, x + yhalf * 56)], fb[(0, x + yhalf * 56 + 28)]);
+        }
+
+        println!("\x1b[0m");
+    }
+}
+
+fn plot(a: f32, b: f32) {
+    let a = (a * 255.0) as u8;
+    let b = (b * 255.0) as u8;
+    print!("\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m▀", a, a, a, b, b, b);
 }
--- mnist/src/reader.rs
+++ mnist/src/reader.rs
@@ -17,7 +17,7 @@ pub fn read_images<R: io::Read>(b: &mut R) -> io::Result<Vec<Vector<784>>> {
 
         for (yi, y) in buf.chunks(28).enumerate() {
             for (xi, i) in y.iter().enumerate() {
-                v[(xi, yi)] = *i as f32 / 255.0;
+                v[(0, xi + yi * 28)] = *i as f32 / 255.0;
             }
         }
 
@@ -25,4 +25,20 @@ pub fn read_images<R: io::Read>(b: &mut R) -> io::Result<Vec<Vector<784>>> {
     }
 
     Ok(images)
+}
+
+pub fn read_labels<R: io::Read>(b: &mut R) -> io::Result<Vec<u8>> {
+    b.read_exact(&mut [0; 4])?;
+
+    let mut size = [0; 4];
+    b.read_exact(&mut size)?;
+
+    let mut labels = Vec::with_capacity(u32::from_be_bytes(size) as usize);
+    let mut buf = [0; 1];
+
+    while let Ok(()) = b.read_exact(&mut buf) {
+        labels.push(buf[0]);
+    }
+
+    Ok(labels)
 }
